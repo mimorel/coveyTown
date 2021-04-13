7f6247e4412d00ad34ca851fe795a7cc6c3a39f6
MI <morel.m@husky.neu.edu>
2021-04-13 16:51:06 -0400

    Update TestUtils.ts
    attempts are being made....
    to fix the bug

--- services/roomService/src/client/TestUtils.ts
+++ services/roomService/src/client/TestUtils.ts
@@ -45,6 +45,8 @@ export function createSocketClient(server: http.Server, sessionToken: string, co
   playerMoved: Promise<RemoteServerPlayer>,
   newPlayerJoined: Promise<RemoteServerPlayer>,
   playerDisconnected: Promise<RemoteServerPlayer>,
+  updateBoardPromise:Promise<RemoteServerPlayer>,
+
 } {
   const address = server.address() as AddressInfo;
   const socket = io(`http://localhost:${address.port}`, {
@@ -75,7 +77,13 @@ export function createSocketClient(server: http.Server, sessionToken: string, co
     socket.on('playerDisconnect', (player: RemoteServerPlayer) => {
       resolve(player);
     });
+
+  const updateBoardPromise = new Promise<RemoteServerPlayer>((resolve) => {
+      socket.on('updateBoard', (board: RemoteServerPlayer) => {
+        resolve(board);
+      });
   });
+
   createdSocketClients.push(socket);
   return {
     socket,
@@ -84,6 +92,7 @@ export function createSocketClient(server: http.Server, sessionToken: string, co
     playerMoved: playerMovedPromise,
     newPlayerJoined: newPlayerPromise,
     playerDisconnected: playerDisconnectPromise,
+    updateBoard:updateBoardPromise,
   };
 }
 export function setSessionTokenAndTownID(coveyTownID: string, sessionToken: string, socket: ServerSocket):void {
